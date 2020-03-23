49961b675f121f9b4f822fa590d9d930c8a697e4
Johnny Delgado <johnny.m.delgado12@gmail.com>
2020-03-22 22:47:17 -0500

    java exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -57,17 +57,17 @@ public class ControlFlowExercises {
 
 //        2. Fizzbuzz
 
-        for(int i = 0; i <= 100; i++ ){
-            if (i % 15 == 0){
-                System.out.println("Fizz Buzz");
-            } else if (i % 5 == 0){
-                System.out.println("Buzz");
-            } else if (i % 3 == 0){
-                System.out.println("Fizz");
-            } else {
-                System.out.println(i);
-            }
-        }
+//        for(int i = 0; i <= 100; i++ ){
+//            if (i % 15 == 0){
+//                System.out.println("Fizz Buzz");
+//            } else if (i % 5 == 0){
+//                System.out.println("Buzz");
+//            } else if (i % 3 == 0){
+//                System.out.println("Fizz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
 
     }
     }
