db3aa70e38402eae1e6ffa23c4e4ffe6847bca63
Johnny Delgado <johnny.m.delgado12@gmail.com>
2020-04-01 13:57:52 -0500

    finished first part of exercise

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,4 +1,46 @@
 package grades;
 
+import java.util.ArrayList;
+
 public class Student {
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(String name){
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+    // returns the student's name
+    public String getName(){
+        return name;
+    };
+    // adds the given grade to the grades property
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+
+    public ArrayList<Integer> getGrade(){
+        return grades;
+    }
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        double sum = 0;
+        for(Integer grade : grades)
+        {
+            sum += grade;
+        }
+        return sum / grades.size();
+    }
+
+    public static void main(String[] args) {
+        Student johnny = new Student("johnny");
+        System.out.println(johnny);
+        System.out.println(johnny.getName());
+        johnny.addGrade(100);
+        johnny.addGrade(97);
+        johnny.addGrade(85);
+        johnny.addGrade(95);
+        System.out.println(johnny.getGrade());
+        System.out.printf("Your grade average is %f, ",johnny.getGradeAverage());
+    }
 }
