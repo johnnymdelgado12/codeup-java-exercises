dc4f7dd1073102f4cdbf2ede25f8bd5e8eb07680
Johnny Delgado <johnny.m.delgado12@gmail.com>
2020-04-08 16:01:01 -0500

    added circle class and circleapp class

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,4 +1,22 @@
 package shapes;
 
+import util.Input;
+
 public class CircleApp {
+
+    static Input input = new Input();
+
+
+    public static void main(String[] args) {
+
+
+        double userInput = input.getDouble();
+        Circle circle = new Circle(userInput);
+        System.out.println(circle.getArea());
+        System.out.println(circle.getCircumference());
+
+    }
+
+
+
 }
